a55eed2511e5a2f61371c4c077ffa04834da9635
Joeph J. Oh <josephjoh31@gmail.com>
2026-06-29 15:21:52 +0900

    vite.config.ts 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 })
